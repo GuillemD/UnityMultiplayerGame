378f21286227989533df9e82e29ea1c43031d5ae
GuillemArman <guillemarman8@gmail.com>
2019-12-13 12:32:04 +0100

    Working On Matchmaking

--- Multiplayer Game Unity/Assets/Scripts/CustomNetworkManager.cs
+++ Multiplayer Game Unity/Assets/Scripts/CustomNetworkManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
 
 public class MsgTypes
 {
+
+
     public const short PlayerPrefabSelect = MsgType.Highest + 1;
     public class PlayerPrefabMsg : MessageBase
     {
@@ -14,6 +19,13 @@ public class MsgTypes
 }
 public class CustomNetworkManager : NetworkManager
 {
+    float time = 9999.0f;
+
+    public Text newMatchName;
+
+    public RectTransform listOfMatches;
+    public GameObject matchEntryPrefab;
+
     public short playerPrefabIndex;
     public override void OnStartServer()
     {
@@ -71,4 +83,118 @@ public class CustomNetworkManager : NetworkManager
         }
     }
 
+
+
+
+   
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 5.0f)
+        {
+            time = 0.0f;
+
+            NetworkManager.singleton.StartMatchMaker();
+            NetworkManager.singleton.matchMaker.ListMatches(0, 10, "", true, 0, 0, OnMatchList);
+
+
+        }
+    }
+
+
+
+    public void OnCreateMatchClicked()
+    {
+        Debug.Log("OnCreateMatchClicked" + newMatchName.text);
+        NetworkManager.singleton.StartMatchMaker();
+        NetworkManager.singleton.matchMaker.CreateMatch(newMatchName.text, 4, true, "", "", "", 0, 0, OnMatchCreate);
+    }
+
+    public void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        NetworkManager.singleton.StopMatchMaker();
+
+        if (success)
+        {
+            NetworkManager.singleton.StartHost(matchInfo);
+
+        }
+        else
+        {
+            Debug.Log("OnMatchCreate failed");
+        }
+
+
+    }
+
+    void OnJoinMatchClicked(UnityEngine.Networking.Types.NetworkID networkId)
+    {
+        NetworkManager.singleton.StartMatchMaker();
+        NetworkManager.singleton.matchMaker.JoinMatch(networkId, "", "", "", 0, 0, OnMatchJoin);
+
+    }
+
+    public void OnMatchJoin(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        NetworkManager.singleton.StopMatchMaker();
+
+        if (success)
+        {
+            NetworkManager.singleton.StartClient(matchInfo);
+        }
+        else
+        {
+            Debug.Log("OnMatchJoin failed");
+        }
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+
+        if (success)
+        {
+            // Destroy previous list
+            int childCount = listOfMatches.transform.childCount;
+            for (int i = 0; i < childCount; ++i)
+            {
+                Destroy(listOfMatches.transform.GetChild(i).gameObject);
+            }
+
+            //Insert new list of matches
+
+            for (int i = 0; i < matches.Count; ++i)
+            {
+                MatchInfoSnapshot match = matches[i];
+                string matchName = match.name;
+                UnityEngine.Networking.Types.NetworkID networkID = match.networkId;
+
+                GameObject gameObject = Instantiate(matchEntryPrefab, listOfMatches);
+                RectTransform rect = gameObject.GetComponent<RectTransform>();
+
+                rect.position = new Vector2(10, listOfMatches.position.y - (float)i * 50.0f);
+
+                Text text = gameObject.GetComponentInChildren<Text>();
+                text.text = "Match: " + matchName;
+                Button button = gameObject.GetComponentInChildren<Button>();
+                button.onClick.AddListener(delegate { OnJoinMatchClicked(networkId); });
+
+
+
+            }
+
+
+
+        }
+
+        else
+        {
+            Debug.Log("OnMatchList failed");
+        }
+
+
+
+
+    }
 }
